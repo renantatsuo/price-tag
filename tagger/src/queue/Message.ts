7b6cf29cf3066b3270b32e8064d4f8aecca3e999
renantatsuo <tatsuorenan@gmail.com>
2020-05-02 19:20:00 -0300

    create queue specification

---
+++ tagger/src/queue/Message.ts
@@ -0,0 +1,14 @@
+/**
+ * Basic message definition.
+ */
+export interface Message {
+  /**
+   * The message date.
+   */
+  date: Date;
+
+  /**
+   * The message payload.
+   */
+  payload: Object;
+}
---
+++ tagger/src/queue/Queue.ts
@@ -0,0 +1,14 @@
+import { Message } from "./Message";
+
+/**
+ * Interface to handle queues operations
+ */
+export interface Queue {
+  /**
+   * Send a message to a queue.
+   *
+   * @param message the message to be published
+   * @param queue the queue to send the message
+   */
+  sendMessage(message: Message, queue?: string): void;
+}
